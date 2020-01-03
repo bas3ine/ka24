aac4473fe2de9ed21afc9e6a1af0f9750d35505c
Atis Yamsook <atisyamsook@Atiss-MacBook-Pro.local>
2020-01-03 23:14:50 +0700

    [Added]:structure Function handleIDForPlay

--- frontend/src/components/Usefull/List.js
+++ frontend/src/components/Usefull/List.js
@@ -3,6 +3,10 @@ import { Card, Menu, Breadcrumb, Button,Typography } from 'antd';
 
 export class List extends Component {
     
+    handleIDForPlay = () =>{
+        console.log(this.props.QueList)
+    }
+
     render() {
         const { Text } = Typography;
         return (
@@ -13,7 +17,7 @@ export class List extends Component {
 <h4 style={{height:"120px"}}>{this.props.QueList.questionName}</h4>
 <hr></hr>
 Create by <Text code>{this.props.QueList.user.nickName} </Text>
-<Button>Play</Button>
+<Button onClick={this.handleIDForPlay}>Play</Button>
           </Card>
         )
     }
